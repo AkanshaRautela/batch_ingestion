8c8e48dcbb0a09dfaa58faef36c9599f4ae5656c
AkanshaRautela <85901755+AkanshaRautela@users.noreply.github.com>
2021-06-17 13:35:39 +0100

    Add files via upload

--- src/main/scala/com/bip/spark/gcp/Context.scala
+++ src/main/scala/com/bip/spark/gcp/Context.scala
@@ -4,10 +4,11 @@ import  org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkConf
 import org.apache.log4j.Logger
 
+
 trait Context {
 
   val sparkConf = new SparkConf().setAppName("Bip_INGESTION_GCP").set("spark.executor.extraClassPath","./")
-  val spark = SparkSession.builder().config(sparkConf).enableHiveSupport().getOrCreate()
+  val spark = SparkSession.builder().config("spark.jars.packages", "com.google.cloud.spark:spark-bigquery_2.12").enableHiveSupport().getOrCreate()
 
   spark.conf.set("hive.exec.dynamic.partition", "true")
   spark.conf.set("hive.exec.dynamic.partition.mode", "nonstrict")
