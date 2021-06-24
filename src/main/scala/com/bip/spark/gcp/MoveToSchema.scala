db19ea0ad41925a8d5c869d0397ab295845c0a24
AkanshaRautela <85901755+AkanshaRautela@users.noreply.github.com>
2021-06-24 22:02:55 +0100

    Add files via upload

--- src/main/scala/com/bip/spark/gcp/MoveToSchema.scala
+++ src/main/scala/com/bip/spark/gcp/MoveToSchema.scala
@@ -2,6 +2,8 @@ package com.bip.spark.gcp
 
 import org.apache.log4j.Logger
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.lit
+import org.apache.spark.sql.types.DataTypes
 
 object MoveToSchema extends Context {
 
@@ -11,15 +13,31 @@ object MoveToSchema extends Context {
 
     logger.info("write df to bq schema table")
 
-//    val viewDF = spark.read.format("bigquery")
-//      .option("table" , "").load().cache()
+    val df = spark.read.format("bigquery")
+                       .option("table", "batch_ingestion_schema.customer_record_table")
+      .load().cache()
 
-    val schemaDF = rawDF.withColumn("deleteFlag","true")
-     .withColumn("Brand_Id","LBG")
-//
-//    schemaDF.show()
-//
-    schemaDF.write.mode("APPEND").format("bigquery").option("temporaryGcsBucket","batch_ingestion_bucket").option("table", "schema_param_table.param_prop_tb").save()
+    val newDF = df.drop("BrandId").drop("DeleteFlag").drop("FileName")
+      .drop("_PARTITIONTIME").drop("_PARTITIONDATE")
+
+    val cols : Array[String] = newDF.columns
+
+    logger.info(" Table schema " + cols.mkString)
+
+    val renamedColsDF = rawDF.toDF(cols:_*)
+
+    renamedColsDF.printSchema()
+
+    val schemaDF = renamedColsDF.withColumn("BrandId",lit("LBG").cast(DataTypes.StringType))
+      .withColumn("DeleteFlag",lit("false").cast(DataTypes.BooleanType))
+      .withColumn("FileName", lit(fileName).cast(DataTypes.StringType))
+
+    schemaDF.show()
+
+    schemaDF.write.mode("APPEND").format("bigquery").partitionBy(date)
+      .option("temporaryGcsBucket","batch_ingestion_bucket")
+      .option("table", "batch_ingestion_schema.customer_record_table")
+      .save()
 
 
 
